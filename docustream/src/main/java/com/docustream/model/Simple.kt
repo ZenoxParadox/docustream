57317123fe55bea5711da4a586186b4ff7bf8d68
Killian Bos <killian@afrogleap.com>
2018-03-28 11:42:06 +0200

    Unittest typeadapter support for gson builder

---
+++ docustream/src/main/java/com/docustream/model/Simple.kt
@@ -0,0 +1,11 @@
+package com.docustream.model
+
+import java.util.Calendar
+
+/**
+ * Created by Killian on 26/02/2018.
+ */
+data class Simple(
+    var contents: String = "1234567890",
+    val calender: Calendar = Calendar.getInstance()
+)
